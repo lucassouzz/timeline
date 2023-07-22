5aac4b43364a0946a342ec2f426a2950851ed9f8
Lucas <lucassouza.zip@gmail.com>
2023-07-22 15:24:28 -0300

    Add user crud and database config

--- src/main/java/com/dev/timeline/user/payload/UserPayload.java
+++ src/main/java/com/dev/timeline/user/payload/UserPayload.java
@@ -1,14 +1,15 @@
 package com.dev.timeline.user.payload;
 
+import com.dev.timeline.user.enumeration.GenderEnum;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-public class UserPayload {
-    private Long id;
+public class UserResponse {
+    private Integer id;
     private String nome;
     private String username;
     private String password;
-    private String gender;
+    private GenderEnum gender;
 }
